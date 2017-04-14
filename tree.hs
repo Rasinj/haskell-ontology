cb5a88310f4b3376b929612696adeac6ce52ccd9
Olle Nordesjö <olle.nordesjo@gmail.com>
2017-04-14 10:52:11 -0400

    Docs

--- tree.hs
+++ tree.hs
@@ -8,10 +8,12 @@ data Node = Node String Int Node
 compare_node :: Node -> Node -> Bool
 compare_node a b = a == b 
 
+-- Get the parent or nothing
 unpack (Node b c d) = d
 unpack (NodeLeaf b c) = Empty 
 unpack Empty = Empty 
 
+-- Accessors
 getLevel (Node s i n) = i
 getLevel (NodeLeaf s i) = i
 getLevel Empty = 0
@@ -19,14 +21,17 @@ getLevel Empty = 0
 getString (Node s i n) = s
 getString (NodeLeaf s i) = s
 
+-- Example data for testing
 myfirstNode = Node "Continuant" 2 (Node "Object" 1 (NodeLeaf "Root" 0 ))
 mysecondNode = Node "Process" 2 (Node "Objict" 1  (NodeLeaf "Root" 0 ))
 
-getLevels a b = map getLevel [a,b]
 
+-- Comparison primitives
+getLevels a b = map getLevel [a,b]
 compareNode args = compare (head args) (last args)
 compareLevels a b = compareNode (getLevels a b)
 
+-- Traversal routines
 findLastCommonLevel a b = case (compareLevels a b) of
                             EQ  ->  case (getString a) == (getString b) of
                                      True -> getString a 
